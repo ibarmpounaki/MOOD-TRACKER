75bd3185ef4ae1c091607a29ed1e3af02060f836
ibarmpounaki <ibarmpounaki@gmail.com>
2026-04-03 20:15:52 +0300

    Left menu implemented toggle functionality (incl the UI)

--- public/js/leftMenu.js
+++ public/js/leftMenu.js
@@ -1,10 +1,31 @@
 document
-  .querySelector(".menu.categories")
+  .querySelector(".part.menu-bar")
   .addEventListener("click", function (e) {
-    console.log(e.target.classList);
-    e.preventDefault();
+    const selectedLink = e.target.closest("a");
+    if (!selectedLink) return;
 
-    if (e.target.classList.contains("journal")) {
-      console.log("JOURNAL");
+    e.preventDefault(); // prevent the default action of the click event
+
+    const selectedImg = selectedLink.querySelector(".menu-image");
+    const selectedCategory = e.target.closest(".menu.categories");
+
+    document.querySelectorAll(".menu-image").forEach((img) => {
+      img.src = img.src.replace("-selected", "-unselected");
+    });
+
+    document.querySelectorAll("a").forEach((a) => {
+      a.style.color = " #5870c06e";
+    });
+
+    document.querySelectorAll(".menu.categories").forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    selectedCategory.classList.add("active");
+
+    // toggle based on current src
+    if (selectedImg.src.includes("-unselected")) {
+      selectedImg.src = selectedImg.src.replace("-unselected", "-selected");
+      selectedLink.style.color = "#2e3d64";
     }
   });
